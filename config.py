44d5b107e00906b5467f8eebbc60b7c7c7d9bc6d
akasazali1 <aka.sazali1@gmail.com>
2026-06-30 17:05:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SUPABASE_URL = os.environ.get('SUPABASE_URL')
-    SUPABASE_KEY = os.environ.get('SUPABASE_KEY')   # This will be your SERVICE ROLE key (for backend admin)
+    SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-change-me')
